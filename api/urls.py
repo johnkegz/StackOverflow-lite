415b6ec3a1f4664ab0b564839d415359eb079583
johnkegz <johnkl24@gmail.com>
2018-08-16 04:31:44 -0700

    Feature(Post Answer): Post Answer to question[#159762059]

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from views import GetQuestion
-class GetUrls():
-    @staticmethod
-    def fetch_urls(question):
-        question_view = GetQuestion.as_view('questions')        
-        question.add_url_rule('/api/v1/questions', view_func=question_view, methods=['POST',])
-         
