995150fa6a721eeddb7548a3bd72e0b0e9030d86
johnkegz <johnkl24@gmail.com>
2018-08-18 02:13:02 -0700

    Chore(Travis): pylint to follow pep8 standards[#159868676]

--- api/routes.py
+++ api/routes.py
@@ -1,11 +1,23 @@
+"""Class for defining url routes"""
 from main_views import GetQuestionAnswer
 class GetRoutes():
+    """
+    GetRoutes defines routes
+    params:urls
+    """
     @staticmethod
     def fetch_routes(questionanswer):
+        """
+        Add url rule defines the routes for http requests
+        """
         question_view = GetQuestionAnswer.as_view('questions')
-        questionanswer.add_url_rule('/api/v1/questions', view_func=question_view, defaults={'question_id': None}, methods=['Get',])        
-        questionanswer.add_url_rule('/api/v1/questions/<int:question_id>', view_func=question_view, methods=['Get',])
+        questionanswer.add_url_rule('/api/v1/questions', view_func=question_view,
+                                    defaults={'question_id': None}, methods=['Get',])
+        questionanswer.add_url_rule('/api/v1/questions/<int:question_id>',
+                                    view_func=question_view, methods=['Get',])
         question_answer = GetQuestionAnswer.as_view('questionsanswers')
-        questionanswer.add_url_rule('/api/v1/questions', view_func=question_answer, methods=['POST',])        
-        questionanswer.add_url_rule('/questions/<int:question_id>/answers', view_func=question_answer, methods=['POST',])
+        questionanswer.add_url_rule('/api/v1/questions',
+                                    view_func=question_answer, methods=['POST',])
+        questionanswer.add_url_rule('/questions/<int:question_id>/answers',
+                                    view_func=question_answer, methods=['POST',])
          
--- api/test_views.py
+++ api/test_views.py
@@ -1,21 +1,40 @@
+"""Module for making tests on the app"""
 import unittest
 import json
-from json import JSONEncoder
 
 from app import App
 class TestViews(unittest.TestCase):
-    def setUp(self):        
+    """"
+    Class for making tests
+    params: unittest.testCase
+    """
+    def setUp(self):
+        """
+        Method for making the client object
+        """
         self.client = App.test_client
     def test_fetch_all_questions(self):
+        """Method for tesing the get function which returns all questions"""
         result = self.client().get('api/v1/questions')
         self.assertTrue(result.json["Questions"])
     def test_get_a_question(self):
-        result = self.client().get('api/v1/questions/1')        
+        """Method for tesing the get function which returns one question"""
+        result = self.client().get('api/v1/questions/1')
         self.assertTrue(result.json["requested question"])
     def test_add_a_questions(self):
-        result = self.client().post('api/v1/questions' , content_type="application/json", data=json.dumps(dict(user_question_id=17, user_name="ben", user_question="am in Gayaza where can i find andela")))
-        self.assertTrue(result.json["New question file"])        
+        """Method for tesing the post function which posts a question"""
+        result = self.client().post('api/v1/questions',
+                                    content_type="application/json",
+                                    data=json.dumps(dict(user_question_id=17, user_name="ben",
+                                                         user_question=
+                                                         "am in Gayaza where can i find andela")))
+        self.assertTrue(result.json["New question file"])
     def test_add_a_answer(self):
-        result = self.client().post('/questions/2/answers' , content_type="application/json", data=json.dumps(dict(answer_id=4, answer="am in Gayaza where can i find andela")))
+        """Method for tesing the post function which posts an answer"""
+        result = self.client().post('/questions/2/answers',
+                                    content_type="application/json",
+                                    data=json.dumps(dict(
+                                        answer_id=4, answer=
+                                        "am in Gayaza where can i find andela")))
         self.assertTrue(result.json["Answer to question"])
         
